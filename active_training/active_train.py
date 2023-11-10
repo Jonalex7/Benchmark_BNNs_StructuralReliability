b2630289c43856e83b50517c199f92f5a6c294b4
Pablo G. Morato <pgmorato@uliege.be>
2023-11-10 17:00:28 +0100

    active training abstraction

---
+++ active_training/active_train.py
@@ -0,0 +1,14 @@
+import torch
+
+class ActiveTrain():
+    def __init__(self):
+        print('engine humming')
+
+    def get_active_points(self, x, x_mc, n_points):
+        y_mean, y_std = torch.mean(y_mc, 1), torch.std(y_mc, 1)
+        sorted = torch.topk(y_std, int(n_points))
+        idx_max_ystd = sorted[1] # Taking the indices of the max std
+        x_new = x_mc[idx_max_ystd]
+        x_next = torch.cat( [x, x_new])
+        return x_next
+
