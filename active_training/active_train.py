4e18b2264d7763198e666bfda516446475f95c96
Pablo G. Morato <pgmorato@uliege.be>
2023-11-10 17:04:49 +0100

    minor edits

--- active_training/active_train.py
+++ active_training/active_train.py
@@ -4,7 +4,7 @@ class ActiveTrain():
     def __init__(self):
         print('engine humming')
 
-    def get_active_points(self, x, x_mc, y_mc n_points):
+    def get_active_points(self, x, x_mc, y_mc, n_points):
         y_mean, y_std = torch.mean(y_mc, 1), torch.std(y_mc, 1)
         sorted = torch.topk(y_std, int(n_points))
         idx_max_ystd = sorted[1] # Taking the indices of the max std
