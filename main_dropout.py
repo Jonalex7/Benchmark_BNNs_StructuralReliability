1796f11bb852575ff4c38fc2e66e403ffcc98cf3
moratodpg <moratosotelo@gmail.com>
2023-11-12 21:38:21 +0100

    minor mod

--- main_dropout.py
+++ main_dropout.py
@@ -24,6 +24,7 @@ net = NeuralNetworkWithDropout(2, 100, 1, 0.1)
 # Active training
 n_train_ep = 20
 active_points = 10
+mcs_samples = int(1e5)
 for ep in range(n_train_ep):
     
     print('Samples: ', X.shape[0])
@@ -32,13 +33,13 @@ for ep in range(n_train_ep):
     #net = NeuralNetworkWithDropout(2, 100, 1, 0.3)
     net.train(train_loader, 1000, 1e-4)
 
-    pf_mc, _, X_mc, Y_mc = lstate.monte_carlo_estimate(1e5)
+    pf_mc, _, X_mc, Y_mc = lstate.monte_carlo_estimate(mcs_samples)
     print('pf ', pf_mc)
     X_uq = torch.tensor(X_mc, dtype=torch.float32)
 
     Y_uq = net.predictive_uq(X_uq, 100)
     Y_mean = Y_uq.mean(dim=1)
-    print('pf_surrogate ' , (((Y_mean<0).sum()) /torch.tensor(int(1e5))).item() )
+    print('pf_surrogate ' , (((Y_mean<0).sum()) /torch.tensor(int(mcs_samples))).item() )
     
     X_uq = torch.tensor(X_uq, dtype=torch.float32)
     X_ = act_train.get_active_points(X, X_uq, Y_uq, active_points)
