4732d85af5116b008a63cf8d198d869ba8440e32
Jonathan <jmoran@uliege.be>
2024-01-08 17:16:26 +0100

    running sghmc with config

---
+++ config/defaults.py
@@ -0,0 +1,26 @@
+# --- Defaults ---
+
+model_config_dict = {
+    "passive_samples": 100,
+    "training_epochs": 100,
+    "network_architecture": (2, 20, 2, 1),
+    "lr": 1e-2,
+    "batch_size": 64
+}
+
+reliability_config_dict = {
+    "mcs_samples": 1e6,
+    "active_points": 5,
+    "active_epochs": 10
+}
+
+# # --- Model configuration ---
+# passive_samples: 10 # Initial samples for DoE
+# training_epochs: 1000 # Number of training epochs 
+# network_architecture: (2, 100, 2, 1) # Input, width, layers, output sizes
+# lr: 0.1 # Learning rate
+# batch_size: 32 # Batch size for each epoch
+# active_points: 5 # Number of new labelled points
+
+# # --- Reliability settings ---
+# mcs_samples: int(1e6) # Size of Monte carlo population
--- main_sghmc.py
+++ main_sghmc.py
@@ -3,30 +3,47 @@ from limit_states.g11d_electric import g11d_electric
 from methods.sghmc import *
 from utils.data import get_dataloader
 from active_training.active_train import ActiveTrain
-from tqdm import tqdm
-
+from config.defaults import *
 import torch.utils.data as data
+import pickle
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Active train BNN with Stochastic Gradient HMC')
+
+parser.add_argument('--res_dir', type=str, nargs='?', action='store', default='results\SGHMC_results',
+                    help='Where to save predicted Pf results. Default: \'results\SGHMC_results\'.')
+parser.add_argument('--res_file', type=str, nargs='?', action='store', default='SGHMC',
+                    help='Pf results file name. Default: \'SGHMC\'.')
+args = parser.parse_args()
+
+# Directory to save results
+results_dir = args.res_dir
+results_file = args.res_file
+
+os.makedirs(results_dir)
 
 lstate = g11d_electric()
 act_train = ActiveTrain()
-pf, beta, _,_, y_mc_test = lstate.monte_carlo_estimate(1e6)
+mcs_samples = int(reliability_config_dict['mcs_samples'])
+pf, beta, _, _, y_mc_test = lstate.monte_carlo_estimate(mcs_samples)
 y_max = np.max(y_mc_test)   #to normalise the output for training
 print('ref PF:', pf, 'B:',beta)
 
 #Passive training
-passive_samples = 100 
+passive_samples = model_config_dict['passive_samples'] 
 x_norm, x_scaled, y_scaled = lstate.get_doe_points(n_samples=passive_samples, method='lhs')
-batch_size = 64
+batch_size = model_config_dict['batch_size']
 
 #network config
-width, layers = 20, 2
+width, layers = model_config_dict['network_architecture'][1], model_config_dict['network_architecture'][2]
+learning_rate = model_config_dict['lr'] 
 
 # Active training
 use_cuda = torch.cuda.is_available()
-n_active_ep = 20
-active_points = 5
-n_passive_ep = 100
-mcs_samples = int(1e6)
+n_active_ep = reliability_config_dict['active_epochs']
+active_points = reliability_config_dict['active_points']
+training_epochs = model_config_dict['training_epochs']
 
 burn_in = 20   #How many epochs to burn in for?. Default: 20.
 sim_steps = 2   #How many epochs pass between saving samples. Default: 2.
@@ -35,19 +52,21 @@ resample_its = 50
 resample_prior_its = 15
 re_burn = 1e8
 
+results_dict = {}
+
 for act_ep in range(n_active_ep):
 
-    x_train = torch.tensor(x_norm, dtype=torch.float32).view(-1,11)
-    y_train = torch.tensor(y_scaled/y_max, dtype=torch.float32).view(-1,1)  #normalised output
+    x_train = torch.tensor(x_norm, dtype=torch.float32).view(-1, lstate.input_dim)
+    y_train = torch.tensor(y_scaled/y_max, dtype=torch.float32).view(-1, lstate.output_dim)  #normalised output
     
     print('Samples: ', x_train.shape[0], end=" ")
     
     train_loader, _ = get_dataloader(x_train, y_train, lstate.input_dim, lstate.output_dim, train_test_split=1.0, batch_size=batch_size)
 
     net = BNN_SGHMC(N_train=len(x_train), input_dim=lstate.input_dim, width=width, depth=layers, output_dim=lstate.output_dim, 
-                lr=1e-2, cuda=use_cuda, grad_std_mul=10)
+                lr=learning_rate, cuda=use_cuda, grad_std_mul=10)
 
-    net.train(train_loader, epoch=n_passive_ep, burn_in=burn_in, re_burn = re_burn , 
+    net.train(train_loader, epoch=training_epochs, burn_in=burn_in, re_burn = re_burn , 
                 resample_its=resample_its, resample_prior_its = resample_prior_its, 
                 sim_steps = sim_steps, N_saves=N_saves, verbose=0)
         
@@ -59,11 +78,16 @@ for act_ep in range(n_active_ep):
     y_bnn_pred = net.sample_predict(X_uq, Nsamples=N_saves)
     y_bnn_mean = torch.mean(y_bnn_pred, 0)
     y_bnn_std = torch.std(y_bnn_pred, 0)
+    pf_sumo = (((y_bnn_mean<0).sum()) / torch.tensor(mcs_samples)).item()
 
-    print('pf_surrogate ' , (((y_bnn_mean<0).sum()) / torch.tensor(mcs_samples)).item() )
+    print('pf_surrogate ' , pf_sumo)
+    results_dict['pf_'+ str(len(x_train))] = pf_sumo #, B_sumo
 
     x_norm = act_train.get_active_points(x_train, X_uq, y_bnn_pred.view(-1, N_saves), active_points)
     x_scaled = lstate.isoprob_transform(x_norm, lstate.marginals)
     y_scaled = lstate.eval_lstate(x_scaled)
+
+results_dict[str(len(x_train))+'_doepoints'] = x_train, y_train
+pickle.dump(results_dict, open(results_dir+'/'+results_file, 'wb'))
 
 print('End training')
